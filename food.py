c7741ec9e9bd0427cc0a3bd6ce246cd7cf3a6583
vishal8202 <111052930+vishal8202@users.noreply.github.com>
2022-11-27 21:22:21 +0530

    total

--- food.py
+++ food.py
@@ -11,7 +11,10 @@ while True:
     print('3 search a recipe')
     print('4 update a recipe')
     print('5 delete a recipe')
-    print('6 exit')
+    print('6 total price of food')
+    print('7 total price for each category')
+    print('8 Starting letter of name of recepi ')
+    print('9 exit')
 
     choice = int(input('Enter an option: '))
     if(choice==1):
@@ -45,18 +48,35 @@ while True:
             print(i)
     elif(choice==4):
         print('update the student')
-        categorys = input('enter the category such as veg or non-veg : ')
-        name = input('enter the name of the recipe : ')
         
+        name = input('enter the name of the recipe to be updated : ')
+        categorys = input('enter the category such as veg or non-veg : ')
         taste = input('enter the taste you need : ')
         price = input('enter the price : ')
 
-        sql = "'"+category+"'"
+        sql = "UPDATE `food` SET `Name`='"+name+"',`Category`='"+categorys+"',`Taste`='"+taste+"',`Price`='"+price+"' WHERE `Category`='"+name+"'"
         mycursor.execute(sql)
-        result = mycursor.fetchall()
-        for i in result:
-            print(i)
+        mydb.commit()
     elif(choice==5):
         print('delete the student')
-    elif(choice==6):
+        name = input('enter the name of the recipe to be deleting : ')
+        sql = "DELETE FROM `food` WHERE `Name`='"+name+"'"
+        mycursor.execute(sql)
+        mydb.commit()
+    elif(choice == 6):
+        print('total price')
+        sql = 'SELECT SUM(`Price`) FROM `food` '
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(result)
+    elif(choice == 7):
+        print('total price for each category')
+        sql = "SELECT `Category`, SUM(`Price`) FROM `food` GROUP BY `Category`"
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        print(result)
+    elif(choice==8):
+        print('Starting letter of name of recepi')
+
+    elif(choice==9):
         break
